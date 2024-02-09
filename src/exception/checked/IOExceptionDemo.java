10e8bd8b0a4ade9f17b8cc848a6760ea10b178e7
danu <danujagreru@gmail.com>
2024-02-09 10:02:00 +0530

    try catch

--- src/exception/checked/IOExceptionDemo.java
+++ src/exception/checked/IOExceptionDemo.java
@@ -10,12 +10,17 @@ import java.io.FileReader;
 
 public class IOExceptionDemo {
     public static void main(String[] args) {
+        System.out.println("Start Main method");
         try {
             var reader = new FileReader("abc.txt");
 
             System.out.println("Huree!!! File found");
         } catch (FileNotFoundException e) {
-            System.out.println("OOPS! File not found");
+            System.out.println(e.getMessage());
+//            e.printStackTrace();
+//            System.out.println("OOPS! File not found");
         }
+
+        System.out.println("End Main method");
     }
 }
